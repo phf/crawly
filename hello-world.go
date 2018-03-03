9c5acdbb422fca19df24eaa086d49b9300833e43
Peter H. Froehlich <peter.hans.froehlich@gmail.com>
2018-03-03 00:50:20 -0500

    Hello with a web server.

--- hello-world.go
+++ hello-world.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func handleRoot(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello World!\n")
+}
 
 func main() {
-    fmt.Println("Hello World")
+	fmt.Println("Hello World!")
+	http.HandleFunc("/", handleRoot)
+	log.Fatal(http.ListenAndServe(":3333", nil))
 }
